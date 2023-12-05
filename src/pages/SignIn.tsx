4ac574baa9bae9ded1b01417b6cefeb6493a67ed
David Silva <davidavilasilva@gmail.com>
2023-12-05 15:42:18 -0800

    Clean up, refine auth

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,23 +1,25 @@
 import { useAuthContext } from "../context/AuthContext";
 import { SignInContextProvider } from "../context/SignInContext";
 import { SignIn as SignInForm } from "../components";
-import { ConfirmSignIn } from "../components";
+import { SignInConfirm } from "../components";
 
 const SignIn = () => {
   const { isAdmin, isLoggedIn } = useAuthContext();
 
+  if (isLoggedIn) {
+    return <p>You are already signed in.</p>;
+  }
+
   return (
-    <div>
+    <>
       <h1>Sign In</h1>
       <p>Are you an admin? {isAdmin ? "yes" : "no"}</p>
       <SignInContextProvider>
         <SignInForm />
 
-        <ConfirmSignIn />
+        <SignInConfirm />
       </SignInContextProvider>
-
-      {/* {(signInStep === "DONE" || isLoggedIn) && <p>You are signed in.</p>} */}
-    </div>
+    </>
   );
 };
 export default SignIn;
