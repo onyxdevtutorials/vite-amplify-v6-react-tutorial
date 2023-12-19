25bf2a544fd1204d3ee8d2777136f3ee147bc857
David Silva <davidavilasilva@gmail.com>
2023-12-19 15:58:06 -0800

    Add tests for AddProduct

---
+++ src/pages/AddProduct.test.tsx
@@ -0,0 +1,89 @@
+import { describe, expect, test, beforeEach, vi } from "vitest";
+import { render, screen } from "@testing-library/react";
+import AddProduct from "./AddProduct";
+import { MemoryRouter } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
+import { createProduct } from "../graphql/mutations";
+
+const { graphqlMock } = vi.hoisted(() => {
+  return { graphqlMock: vi.fn() };
+});
+
+vi.mock("aws-amplify/api", () => ({
+  generateClient: () => ({
+    graphql: graphqlMock,
+  }),
+}));
+
+describe("AddProduct", () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  test("renders Add Product form", () => {
+    render(
+      <MemoryRouter>
+        <AddProduct />
+      </MemoryRouter>
+    );
+
+    // Assert that the form elements are rendered
+    expect(
+      screen.getByRole("textbox", { name: /product name/i })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("textbox", { name: /description/i })
+    ).toBeInTheDocument();
+    expect(screen.getByRole("textbox", { name: /price/i })).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: /add/i })).toBeInTheDocument();
+  });
+
+  test("displays validation errors when form is submitted with invalid data", async () => {
+    const user = userEvent.setup();
+
+    render(
+      <MemoryRouter>
+        <AddProduct />
+      </MemoryRouter>
+    );
+
+    // Submit the form without filling in any fields
+    await user.click(screen.getByRole("button", { name: /add/i }));
+
+    const requiredInputs = screen.getAllByText("Required");
+
+    expect(requiredInputs).toHaveLength(3);
+  });
+
+  test.only("submits the form with valid data and calls graphql()", async () => {
+    const user = userEvent.setup();
+
+    render(
+      <MemoryRouter>
+        <AddProduct />
+      </MemoryRouter>
+    );
+
+    // Fill in the form fields
+    await user.type(screen.getByLabelText("Product Name"), "Test Product");
+    await user.type(screen.getByLabelText("Description"), "Test Description");
+    await user.type(screen.getByLabelText("Price"), "10.99");
+
+    // Submit the form
+    await user.click(screen.getByRole("button", { name: "Add" }));
+
+    expect(graphqlMock).toHaveBeenCalledWith({
+      query: createProduct,
+      variables: {
+        input: {
+          name: "Test Product",
+          description: "Test Description",
+          price: "10.99",
+        },
+      },
+    });
+
+    // Assert that the success toast is displayed
+    expect(screen.getByText("Product added successfully")).toBeInTheDocument();
+  });
+});
