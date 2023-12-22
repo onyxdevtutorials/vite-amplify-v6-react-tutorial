b7ff7c2a26193bf4686377dc36a992d046ff49ab
David Silva <davidavilasilva@gmail.com>
2023-12-22 12:12:48 -0800

    Make SignUp and SignUpConfirm pages
    Instead of having the SignUp and SignUpConfirm components, we will make them pages. This will allow us to use the react-router-dom to navigate between them.

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,5 +2,3 @@ export { default as ListProducts } from "./ListProducts";
 export { default as Banner } from "./Banner";
 export { default as SignInConfirm } from "./SignInConfirm";
 export { default as SignIn } from "./SignIn";
-export { default as SignUp } from "./SignUp";
-export { default as SignUpConfirm } from "./SignUpConfirm";
