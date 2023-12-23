ab4a288d48adcdbef9163618040638c321b0c8b1
David Silva <davidavilasilva@gmail.com>
2023-12-23 15:54:07 -0800

    Begin separating product into its own component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,3 +2,4 @@ export { default as ListProducts } from "./ListProducts";
 export { default as Banner } from "./Banner";
 export { default as SignInConfirm } from "./SignInConfirm";
 export { default as SignIn } from "./SignIn";
+export { default as Product } from "./Product";
