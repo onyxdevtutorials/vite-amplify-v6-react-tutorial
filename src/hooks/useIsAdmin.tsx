7136811348100a934bfe2801c1625b03fa80b4bb
David Silva <davidavilasilva@gmail.com>
2023-12-28 15:42:29 -0800

    Store isAdmin in localStorage

--- src/hooks/useIsAdmin.tsx
+++ src/hooks/useIsAdmin.tsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { AuthError, fetchAuthSession } from "aws-amplify/auth";
 
 const useIsAdmin = () => {
-  const [isAdmin, setIsAdmin] = useState(false);
+  const initialIsAdmin = localStorage.getItem("isAdmin") === "true";
+  const [isAdmin, setIsAdmin] = useState(initialIsAdmin);
 
-  const checkIsAdmin = async () => {
+  const checkIsAdmin = useCallback(async () => {
     try {
       const session = await fetchAuthSession();
       const tokens = session.tokens;
@@ -12,15 +13,21 @@ const useIsAdmin = () => {
         const groups = tokens.accessToken.payload["cognito:groups"];
         if (groups && Array.isArray(groups) && groups.includes("admin")) {
           setIsAdmin(true);
+          localStorage.setItem("isAdmin", "true");
         } else {
           setIsAdmin(false);
+          localStorage.setItem("isAdmin", "false");
         }
       }
     } catch (error) {
       const authError = error as AuthError;
       console.error(`Error checking admin status: ${authError.message}`);
     }
-  };
+  }, []);
+
+  useEffect(() => {
+    checkIsAdmin();
+  }, [checkIsAdmin]);
 
   return { isAdmin, checkIsAdmin };
 };
