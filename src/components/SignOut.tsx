b440e773c36e94deebf22f7d85a744bc1c56b553
David Silva <davidavilasilva@gmail.com>
2023-12-28 17:05:22 -0800

    Add tests for SignOut component

--- src/components/SignOut.tsx
+++ src/components/SignOut.tsx
@@ -1,20 +1,23 @@
-import { signOut } from "aws-amplify/auth";
+import { AuthError, signOut } from "aws-amplify/auth";
 import { useNavigate } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import { useAuthContext } from "../context/AuthContext";
 
 const SignOut = () => {
-  const { setIsLoggedIn } = useAuthContext();
+  const { setIsLoggedIn, setIsAdmin } = useAuthContext();
   const navigate = useNavigate();
 
   const handleSignOut = async () => {
     try {
       await signOut();
       setIsLoggedIn(false);
+      setIsAdmin(false);
       localStorage.removeItem("isLoggedIn");
+      localStorage.removeItem("isAdmin");
       navigate("/");
     } catch (error) {
-      console.error("could not sign out", error);
+      const authError = error as AuthError;
+      console.error("could not sign out", authError);
     }
   };
 
