b7acece77b7f30c112424460983dd30f89d5db00
David Silva <davidavilasilva@gmail.com>
2023-12-28 20:49:15 -0800

    Add tests for useCheckForUser hook

---
+++ src/hooks/useCheckForUser.test.tsx
@@ -0,0 +1,35 @@
+import { renderHook, act, waitFor } from "@testing-library/react";
+import { getCurrentUser } from "aws-amplify/auth";
+import useCheckForUser from "./useCheckForUser";
+import { describe, test, expect, vi } from "vitest";
+
+vi.mock("aws-amplify/auth");
+
+describe("useCheckForUser", () => {
+  test("should set isLoggedIn to true when getCurrentUser resolves", async () => {
+    vi.mocked(getCurrentUser).mockResolvedValue({
+      username: "test",
+      userId: "123",
+    });
+
+    const { result } = renderHook(() => useCheckForUser());
+
+    act(() => {
+      result.current.checkUser();
+    });
+
+    await waitFor(() => expect(result.current.isLoggedIn).toBe(true));
+  });
+
+  test("should set isLoggedIn to false when getCurrentUser rejects", async () => {
+    vi.mocked(getCurrentUser).mockRejectedValue(new Error("Error"));
+
+    const { result } = renderHook(() => useCheckForUser());
+
+    act(() => {
+      result.current.checkUser();
+    });
+
+    await waitFor(() => expect(result.current.isLoggedIn).toBe(false));
+  });
+});
