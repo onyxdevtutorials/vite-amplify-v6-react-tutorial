e5aaa3e69ad5cc21907f3daba8be272bb31f194a
David Silva <davidavilasilva@gmail.com>
2024-01-03 16:47:51 -0800

    Add useGetProduct hook

---
+++ src/hooks/useGetProduct.tsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+import { getProduct } from "../graphql/queries";
+import { generateClient, GraphQLResult } from "aws-amplify/api";
+import useCheckForUser from "../hooks/useCheckForUser";
+import { GetProductQuery } from "../API";
+
+const client = generateClient();
+
+const useGetProduct = (productId: string) => {
+  const [product, setProduct] = useState<GetProductQuery["getProduct"]>(null);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const { isLoggedIn } = useCheckForUser();
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      if (!productId) {
+        console.error("no product id provided");
+        return;
+      }
+
+      try {
+        const result = (await client.graphql({
+          query: getProduct,
+          variables: { id: productId },
+          authMode: isLoggedIn ? "userPool" : "iam",
+        })) as GraphQLResult<GetProductQuery>;
+        console.log("result: ", result);
+        const productData = result.data?.getProduct;
+        // console.log("productData: ", productData);
+        if (!productData || result.errors) {
+          setErrorMessage("Could not get product with ID: " + productId);
+          return;
+        }
+
+        setProduct(productData);
+      } catch (err) {
+        console.error("error fetching product: ", err);
+        setErrorMessage("Error fetching product with ID: " + productId);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchProduct();
+  }, [productId, isLoggedIn]);
+
+  return { product, errorMessage, isLoading };
+};
+
+export default useGetProduct;
