b444083276474a649fba9d8342153d462a2dab14
David Silva <davidavilasilva@gmail.com>
2024-01-10 16:08:47 -0800

    Work on persisting image selection

--- src/pages/EditProduct.tsx
+++ src/pages/EditProduct.tsx
@@ -1,21 +1,13 @@
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
-import Alert from "react-bootstrap/Alert";
 import { toast } from "react-toastify";
 import { useState, useEffect } from "react";
-import { useFormik } from "formik";
-import * as yup from "yup";
+import { FormikValues, useFormik } from "formik";
 import { Link, useParams } from "react-router-dom";
 import { generateClient, GraphQLResult } from "aws-amplify/api";
 import { updateProduct } from "../graphql/mutations";
 import { getProduct } from "../graphql/queries";
 import { GetProductQuery } from "../API";
-
-const validationSchema = yup.object().shape({
-  name: yup.string().required("Required"),
-  description: yup.string().required("Required"),
-  price: yup.string().required("Required"),
-});
+import useGetProduct from "../hooks/useGetProduct";
+import ProductForm from "../components/ProductForm";
 
 type Product = {
   name: string;
@@ -27,82 +19,49 @@ type Product = {
 const client = generateClient();
 
 const EditProduct = () => {
-  const [errorMessage, setErrorMessage] = useState("");
-  const [loading, setLoading] = useState(true);
   const { productId } = useParams<{ productId: string }>();
+  const {
+    product,
+    errorMessage: getProductErrorMessage,
+    isLoading,
+  } = useGetProduct(productId);
 
-  console.log("Edit Product page productId: ", productId);
+  console.log("Edit Product page productId: ", product);
   const initialFormValues = {
-    name: "",
-    description: "",
-    price: "",
-    id: "",
+    name: product?.name || "",
+    description: product?.description || "",
+    price: product?.price || "",
   };
 
-  const onSubmit = async (values: Product) => {
+  const onSubmit = async (values: FormikValues) => {
     if (!productId) return;
-    const { name, description, price } = values;
-    const product = { name, description, price, id: productId };
+    const { name, description, price, image } = values;
+    const productData = {
+      name,
+      description,
+      price,
+      image: image || product?.image,
+      id: productId,
+    };
     try {
       await client.graphql({
         query: updateProduct,
         variables: {
-          input: product,
+          input: productData,
         },
       });
       toast.success("Product updated successfully");
     } catch (err) {
       console.error("error updating product: ", err);
-      setErrorMessage("Error updating product id: " + productId);
+      toast.error("Error updating product");
     }
   };
 
-  const {
-    values,
-    errors,
-    touched,
-    isSubmitting,
-    handleBlur,
-    handleChange,
-    handleSubmit,
-    resetForm,
-  } = useFormik({
-    initialValues: initialFormValues,
-    validationSchema,
-    onSubmit,
-  });
+  const handleRemoveImage = async () => {
+    // to come
+  };
 
-  useEffect(() => {
-    const fetchProduct = async () => {
-      try {
-        if (!productId) return;
-        const result: GraphQLResult<GetProductQuery> = await client.graphql({
-          query: getProduct,
-          variables: { id: productId },
-        });
-        const productData = result.data?.getProduct;
-        if (!productData) {
-          setErrorMessage("Error getting product");
-          return;
-        }
-        const name = productData.name || "";
-        const description = productData.description || "";
-        const price = productData.price || "";
-        const id = productData.id || "";
-        const formValues = { name, description, price, id };
-        console.log("formValues: ", formValues);
-        resetForm({ values: formValues });
-      } catch (err) {
-        console.error("error getting product:", err);
-        setErrorMessage("Error getting product");
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchProduct();
-  }, [productId, resetForm]);
-
-  if (loading) {
+  if (isLoading) {
     return (
       <>
         <Link to="/">List Products</Link>
@@ -116,60 +75,14 @@ const EditProduct = () => {
     <>
       <Link to="/">List Products</Link>
       <h1>Edit Product</h1>
-      <Form onSubmit={handleSubmit} noValidate aria-label="edit product form">
-        <Form.Group controlId="productName">
-          <Form.Label>Product Name</Form.Label>
-          <Form.Control
-            type="text"
-            name="name"
-            value={values.name}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            isInvalid={!!errors.name && touched.name}
-            required
-          />
-          <Form.Control.Feedback type="invalid">
-            {errors.name}
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group controlId="productDescription">
-          <Form.Label>Description</Form.Label>
-          <Form.Control
-            as="textarea"
-            name="description"
-            value={values.description}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            isInvalid={!!errors.description && touched.description}
-            required
-          />
-          <Form.Control.Feedback type="invalid">
-            {errors.description}
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group controlId="productPrice">
-          <Form.Label>Price</Form.Label>
-          <Form.Control
-            type="text"
-            name="price"
-            value={values.price}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            isInvalid={!!errors.price && touched.price}
-            required
-          />
-          <Form.Control.Feedback type="invalid">
-            {errors.price}
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Button type="submit" variant="primary" disabled={isSubmitting}>
-          Update
-        </Button>
-      </Form>
-      <Alert variant="danger" show={!!errorMessage}>
-        {errorMessage}
-      </Alert>
+      <ProductForm
+        initialValues={initialFormValues}
+        onSubmit={onSubmit}
+        initialImageKey={product?.image || undefined}
+        onRemoveImage={handleRemoveImage}
+      />
     </>
   );
 };
+
 export default EditProduct;
