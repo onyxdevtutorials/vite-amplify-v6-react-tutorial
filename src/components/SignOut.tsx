1d3d1997d31ee5e24d454ee5d051401599e1451f
David Silva <davidavilasilva@gmail.com>
2024-01-12 19:02:27 -0800

    More refactoring of AuthContext Putting all the auth logic into the AuthContext

--- src/components/SignOut.tsx
+++ src/components/SignOut.tsx
@@ -1,20 +1,15 @@
-import { AuthError, signOut } from "aws-amplify/auth";
+import { AuthError } from "aws-amplify/auth";
 import { useNavigate } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import { useAuthContext } from "../context/AuthContext";
 
 const SignOut = () => {
-  const { setIsLoggedIn, setIsAdmin } = useAuthContext();
+  const { signOut } = useAuthContext();
   const navigate = useNavigate();
 
   const handleSignOut = async () => {
     try {
-      await signOut();
-      setIsLoggedIn(false);
-      setIsAdmin(false);
-      localStorage.removeItem("isLoggedIn");
-      localStorage.removeItem("isAdmin");
-      navigate("/");
+      await signOut(navigate);
     } catch (error) {
       const authError = error as AuthError;
       console.error("could not sign out", authError);
