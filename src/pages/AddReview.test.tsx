4c745502ebcca502217d9a3fbdd1b24702912130
David Silva <davidavilasilva@gmail.com>
2024-01-17 12:09:11 -0800

    Update AddReview and tests Following AuthContext refactor

--- src/pages/AddReview.test.tsx
+++ src/pages/AddReview.test.tsx
@@ -5,8 +5,41 @@ import { MemoryRouter } from "react-router-dom";
 import userEvent from "@testing-library/user-event";
 import { createReview } from "../graphql/mutations";
 import { toast } from "react-toastify";
-import useCheckForUser from "../hooks/useCheckForUser";
 import useGetProduct from "../hooks/useGetProduct";
+import { AuthContextProvider } from "../context/AuthContext";
+
+vi.mock("aws-amplify/auth");
+
+const { useAuthContextMock } = vi.hoisted(() => {
+  return {
+    useAuthContextMock: vi.fn().mockReturnValue({
+      isLoggedIn: true,
+      signInStep: "",
+      setSignInStep: vi.fn(),
+      isAdmin: false,
+      user: {
+        userId: "1234",
+        username: "testuser",
+      },
+      signIn: vi.fn(),
+      signOut: vi.fn(),
+      signUp: vi.fn(),
+      confirmSignUp: vi.fn(),
+      confirmSignIn: vi.fn(),
+      resetAuthState: vi.fn(),
+    }),
+  };
+});
+
+vi.mock("../context/AuthContext", async () => {
+  const actual = await vi.importActual<typeof import("../context/AuthContext")>(
+    "../context/AuthContext"
+  );
+  return {
+    ...actual,
+    useAuthContext: useAuthContextMock,
+  };
+});
 
 const { graphqlMock } = vi.hoisted(() => {
   return { graphqlMock: vi.fn() };
@@ -25,8 +58,6 @@ vi.mock("react-toastify", () => ({
 }));
 
 vi.mock("../hooks/useGetProduct");
-
-vi.mock("../hooks/useCheckForUser");
 
 vi.mock("react-router-dom", async () => {
   const router = await vi.importActual<typeof import("react-router-dom")>(
@@ -50,7 +81,9 @@ const mockReview = {
 const renderAddReview = () => {
   return render(
     <MemoryRouter>
-      <AddReview />
+      <AuthContextProvider>
+        <AddReview />
+      </AuthContextProvider>
     </MemoryRouter>
   );
 };
@@ -61,63 +94,6 @@ describe("AddReview", () => {
   });
 
   test("renders AddReview form", async () => {
-    vi.mocked(useGetProduct).mockImplementationOnce((productId) => {
-      if (productId === "372db325-5f72-49fa-ba8c-ab628c0ed470") {
-        return {
-          product: {
-            __typename: "Product",
-            name: "Test Product",
-            description: "Test Description",
-            price: "10.99",
-            id: "372db325-5f72-49fa-ba8c-ab628c0ed470",
-            createdAt: "2021-08-01T00:00:00.000Z",
-            updatedAt: "2021-08-01T00:00:00.000Z",
-            isArchived: false,
-            reviews: {
-              nextToken: null,
-              __typename: "ModelReviewConnection",
-            },
-          },
-          isLoading: false,
-          errorMessage: "",
-        };
-      } else {
-        return {
-          product: null,
-          isLoading: false,
-          errorMessage: "No product found",
-        };
-      }
-    });
-
-    vi.mocked(useCheckForUser).mockReturnValueOnce({
-      user: {
-        userId: "1234",
-        username: "testuser",
-      },
-      isLoggedIn: true,
-      checkUser: vi.fn(),
-    });
-
-    renderAddReview();
-
-    await waitFor(() => {
-      expect(screen.getByText("Test Product")).toBeInTheDocument();
-    });
-
-    const ratingInput = screen.getByLabelText(/rating/i);
-    const contentInput = screen.getByRole("textbox", { name: /review/i });
-    const button = screen.getByRole("button", { name: /submit/i });
-
-    expect(ratingInput).toBeInTheDocument();
-    expect(contentInput).toBeInTheDocument();
-    expect(button).toBeInTheDocument();
-  });
-
-  test("displays validation errors when form is submitted with invalid data", async () => {
-    const user = userEvent.setup();
-
-    // Test fails if we use mockImplementationOnce() here
     vi.mocked(useGetProduct).mockImplementation((productId) => {
       if (productId === "372db325-5f72-49fa-ba8c-ab628c0ed470") {
         return {
@@ -147,16 +123,6 @@ describe("AddReview", () => {
       }
     });
 
-    // Test fails if we use mockReturnValueOnce() here
-    vi.mocked(useCheckForUser).mockResolvedValue({
-      user: {
-        userId: "1234",
-        username: "testuser",
-      },
-      isLoggedIn: true,
-      checkUser: vi.fn(),
-    });
-
     renderAddReview();
 
     await waitFor(() => {
@@ -167,23 +133,15 @@ describe("AddReview", () => {
     const contentInput = screen.getByRole("textbox", { name: /review/i });
     const button = screen.getByRole("button", { name: /submit/i });
 
-    await user.clear(ratingInput);
-    await user.clear(contentInput);
-
-    // Submit the form without filling in any fields
-    // Actually, submit button is disabled until rating and content are filled in
-    await user.click(button);
-    expect(button).toBeDisabled();
-
-    const requiredInputs = screen.getAllByText("Required");
-
-    expect(requiredInputs.length).toBe(2);
+    expect(ratingInput).toBeInTheDocument();
+    expect(contentInput).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
   });
 
-  test("calls createReview mutation with correct variables when form is submitted with valid data", async () => {
+  test("displays validation errors when form is submitted with invalid data", async () => {
     const user = userEvent.setup();
 
-    vi.mocked(useGetProduct).mockImplementationOnce((productId) => {
+    vi.mocked(useGetProduct).mockImplementation((productId) => {
       if (productId === "372db325-5f72-49fa-ba8c-ab628c0ed470") {
         return {
           product: {
@@ -212,14 +170,59 @@ describe("AddReview", () => {
       }
     });
 
-    // Test fails if we use mockReturnValueOnce() here
-    vi.mocked(useCheckForUser).mockReturnValue({
-      user: {
-        userId: "1234",
-        username: "testuser",
-      },
-      isLoggedIn: true,
-      checkUser: vi.fn(),
+    renderAddReview();
+
+    await waitFor(() => {
+      expect(screen.getByText("Test Product")).toBeInTheDocument();
+    });
+
+    const ratingInput = screen.getByLabelText(/rating/i);
+    const contentInput = screen.getByRole("textbox", { name: /review/i });
+    const button = screen.getByRole("button", { name: /submit/i });
+
+    await user.clear(ratingInput);
+    await user.clear(contentInput);
+
+    // Submit the form without filling in any fields
+    // Actually, submit button should be disabled until rating and content are filled in
+    await user.click(button);
+    expect(button).toBeDisabled();
+
+    const requiredInputs = screen.getAllByText("Required");
+
+    expect(requiredInputs.length).toBe(2);
+  });
+
+  test("calls createReview mutation with correct variables when form is submitted with valid data", async () => {
+    const user = userEvent.setup();
+
+    vi.mocked(useGetProduct).mockImplementation((productId) => {
+      if (productId === "372db325-5f72-49fa-ba8c-ab628c0ed470") {
+        return {
+          product: {
+            __typename: "Product",
+            name: "Test Product",
+            description: "Test Description",
+            price: "10.99",
+            id: "372db325-5f72-49fa-ba8c-ab628c0ed470",
+            createdAt: "2021-08-01T00:00:00.000Z",
+            updatedAt: "2021-08-01T00:00:00.000Z",
+            isArchived: false,
+            reviews: {
+              nextToken: null,
+              __typename: "ModelReviewConnection",
+            },
+          },
+          isLoading: false,
+          errorMessage: "",
+        };
+      } else {
+        return {
+          product: null,
+          isLoading: false,
+          errorMessage: "No product found",
+        };
+      }
     });
 
     vi.mocked(graphqlMock).mockResolvedValueOnce({
@@ -254,7 +257,7 @@ describe("AddReview", () => {
   test("displays success message when review is added successfully", async () => {
     const user = userEvent.setup();
 
-    vi.mocked(useGetProduct).mockImplementationOnce((productId) => {
+    vi.mocked(useGetProduct).mockImplementation((productId) => {
       if (productId === "372db325-5f72-49fa-ba8c-ab628c0ed470") {
         return {
           product: {
@@ -283,15 +286,6 @@ describe("AddReview", () => {
       }
     });
 
-    vi.mocked(useCheckForUser).mockReturnValueOnce({
-      user: {
-        userId: "1234",
-        username: "testuser",
-      },
-      isLoggedIn: true,
-      checkUser: vi.fn(),
-    });
-
     vi.mocked(graphqlMock).mockResolvedValueOnce({
       data: {
         createReview: mockReview,
@@ -314,5 +308,56 @@ describe("AddReview", () => {
     await waitFor(() => expect(graphqlMock).toHaveBeenCalled());
 
     expect(toast.success).toHaveBeenCalledWith("Review added successfully");
+  });
+
+  test("should not show review form if user is not logged in", async () => {
+    vi.mocked(useAuthContextMock).mockReturnValue({
+      isLoggedIn: false,
+      signInStep: "",
+      setSignInStep: vi.fn(),
+      isAdmin: false,
+      user: null,
+      signIn: vi.fn(),
+      signOut: vi.fn(),
+      signUp: vi.fn(),
+      confirmSignUp: vi.fn(),
+      confirmSignIn: vi.fn(),
+      resetAuthState: vi.fn(),
+    });
+
+    vi.mocked(useGetProduct).mockImplementation((productId) => {
+      if (productId === "372db325-5f72-49fa-ba8c-ab628c0ed470") {
+        return {
+          product: {
+            __typename: "Product",
+            name: "Test Product",
+            description: "Test Description",
+            price: "10.99",
+            id: "372db325-5f72-49fa-ba8c-ab628c0ed470",
+            createdAt: "2021-08-01T00:00:00.000Z",
+            updatedAt: "2021-08-01T00:00:00.000Z",
+            isArchived: false,
+            reviews: {
+              nextToken: null,
+              __typename: "ModelReviewConnection",
+            },
+          },
+          isLoading: false,
+          errorMessage: "",
+        };
+      } else {
+        return {
+          product: null,
+          isLoading: false,
+          errorMessage: "No product found",
+        };
+      }
+    });
+
+    renderAddReview();
+
+    expect(
+      screen.getByText("Please sign in to add a review")
+    ).toBeInTheDocument();
   });
 });
