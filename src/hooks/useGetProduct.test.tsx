ffa7029fc7275d403beec8c18097828994c784ca
David Silva <davidavilasilva@gmail.com>
2024-01-17 11:37:04 -0800

    Update useGetProduct.test.tsx Get signed-in status from context

--- src/hooks/useGetProduct.test.tsx
+++ src/hooks/useGetProduct.test.tsx
@@ -16,12 +16,29 @@ vi.mock("aws-amplify/api", () => ({
   })),
 }));
 
-vi.mock("../hooks/useCheckForUser", () => {
+const { useAuthContextMock } = vi.hoisted(() => {
   return {
-    __esModule: true,
-    default: vi.fn(() => ({
+    useAuthContextMock: vi.fn().mockReturnValue({
       isLoggedIn: true,
-    })),
+      signInStep: "",
+      setSignInStep: vi.fn(),
+      isAdmin: false,
+      user: null,
+      signIn: vi.fn(),
+      signOut: vi.fn(),
+      signUp: vi.fn(),
+      confirmSignUp: vi.fn(),
+      confirmSignIn: vi.fn(),
+      resetAuthState: vi.fn(),
+    }),
+  };
+});
+
+vi.mock("../context/AuthContext", async () => {
+  const actual = await import("../context/AuthContext");
+  return {
+    ...actual,
+    useAuthContext: useAuthContextMock,
   };
 });
 
@@ -37,7 +54,7 @@ describe("useGetProduct", () => {
     vi.clearAllMocks();
   });
 
-  test("should return product when getProduct resolves", async () => {
+  test("should return product when getProduct resolves for logged in user", async () => {
     vi.mocked(graphqlMock).mockResolvedValueOnce({
       data: {
         getProduct: mockProduct,
@@ -80,5 +97,39 @@ describe("useGetProduct", () => {
     expect(result.current.errorMessage).toBe(
       "Error fetching product with ID: 372db325-5f72-49fa-ba8c-ab628c0ed470"
     );
+  });
+
+  test("should return product even for anonymous user", async () => {
+    vi.mocked(useAuthContextMock).mockReturnValueOnce({
+      isLoggedIn: false,
+      signInStep: "",
+      setSignInStep: vi.fn(),
+      isAdmin: false,
+      user: null,
+      signIn: vi.fn(),
+      signOut: vi.fn(),
+      signUp: vi.fn(),
+      confirmSignUp: vi.fn(),
+      confirmSignIn: vi.fn(),
+      resetAuthState: vi.fn(),
+    });
+    vi.mocked(graphqlMock).mockResolvedValueOnce({
+      data: {
+        getProduct: mockProduct,
+      },
+    });
+
+    const wrapper = ({ children }: { children?: ReactNode }) => (
+      <AuthContextProvider>{children}</AuthContextProvider>
+    );
+
+    const { result } = renderHook(
+      () => useGetProduct("372db325-5f72-49fa-ba8c-ab628c0ed470"),
+      { wrapper }
+    );
+
+    await waitFor(() => result.current.product !== null);
+
+    expect(result.current.product).toEqual(mockProduct);
   });
 });
