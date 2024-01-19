79747252af0a539ec87bb26d12113d692736b333
David Silva <davidavilasilva@gmail.com>
2024-01-18 16:21:34 -0800

    Update after AuthContext changes

--- src/pages/ChangePassword.test.tsx
+++ src/pages/ChangePassword.test.tsx
@@ -1,10 +1,11 @@
 import { describe, expect, test, vi, beforeEach } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import ChangePassword from "./ChangePassword";
 import userEvent from "@testing-library/user-event";
 import * as awsAmplifyAuth from "aws-amplify/auth";
 import { toast } from "react-toastify";
+import { AuthContextProvider } from "../context/AuthContext";
 
 vi.mock("aws-amplify/auth");
 
@@ -14,125 +15,126 @@ vi.mock("react-toastify", () => ({
   },
 }));
 
-const renderChangePassword = () => {
-  render(
-    <MemoryRouter>
-      <ChangePassword />
-    </MemoryRouter>
-  );
+const renderChangePassword = async () => {
+  await waitFor(async () => {
+    render(
+      <MemoryRouter>
+        <AuthContextProvider>
+          <ChangePassword />
+        </AuthContextProvider>
+      </MemoryRouter>
+    );
+  });
 };
 
 describe("ChangePassword", () => {
-  beforeEach(() => {
-    vi.clearAllMocks();
+  describe("success path", () => {
+    beforeEach(async () => {
+      vi.clearAllMocks();
+
+      vi.mocked(awsAmplifyAuth.getCurrentUser).mockResolvedValue({
+        username: "testuser",
+        userId: "123",
+      });
+
+      vi.mocked(awsAmplifyAuth.updatePassword).mockResolvedValue();
+
+      await renderChangePassword();
+    });
+
+    test("renders Change Password form", () => {
+      expect(
+        screen.getByRole("heading", { level: 1, name: /change password/i })
+      ).toBeInTheDocument();
+      expect(
+        screen.getByRole("textbox", { name: /username/i })
+      ).toBeInTheDocument();
+      expect(screen.getByLabelText("Old Password")).toBeInTheDocument();
+      expect(screen.getByLabelText("New Password")).toBeInTheDocument();
+      expect(
+        screen.getByRole("button", { name: "Change Password" })
+      ).toBeInTheDocument();
+    });
+
+    test("updates password successfully", async () => {
+      const user = userEvent.setup();
+
+      const oldPasswordInput = screen.getByLabelText("Old Password");
+      const newPasswordInput = screen.getByLabelText("New Password");
+      const changePasswordButton = screen.getByRole("button", {
+        name: /change password/i,
+      });
+
+      await user.type(oldPasswordInput, "oldpassword");
+      await user.type(newPasswordInput, "newpassword");
+
+      await user.click(changePasswordButton);
+
+      expect(awsAmplifyAuth.updatePassword).toHaveBeenCalledTimes(1);
+      expect(awsAmplifyAuth.updatePassword).toHaveBeenCalledWith({
+        oldPassword: "oldpassword",
+        newPassword: "newpassword",
+      });
+
+      expect(vi.mocked(toast.success)).toHaveBeenCalledWith(
+        "Password changed successfully"
+      );
+    });
   });
+  describe("error handling", () => {
+    beforeEach(async () => {
+      vi.clearAllMocks();
 
-  test("renders Change Password form", () => {
-    renderChangePassword();
+      const errorMessage = "Incorrect username or password.";
 
-    expect(
-      screen.getByRole("heading", { level: 1, name: /change password/i })
-    ).toBeInTheDocument();
-    expect(
-      screen.getByRole("textbox", { name: /username/i })
-    ).toBeInTheDocument();
-    expect(screen.getByLabelText("Old Password")).toBeInTheDocument();
-    expect(screen.getByLabelText("New Password")).toBeInTheDocument();
-    expect(
-      screen.getByRole("button", { name: "Change Password" })
-    ).toBeInTheDocument();
-  });
+      vi.mocked(awsAmplifyAuth.getCurrentUser).mockResolvedValueOnce({
+        username: "testuser",
+        userId: "123",
+      });
 
-  test("updates password successfully", async () => {
-    const user = userEvent.setup();
+      vi.mocked(awsAmplifyAuth.updatePassword).mockRejectedValueOnce({
+        message: errorMessage,
+      });
 
-    // Tests seem to hang indefinitely if use mockResolvedValueOnce
-    vi.mocked(awsAmplifyAuth.getCurrentUser).mockResolvedValue({
-      username: "testuser",
-      userId: "123",
+      await renderChangePassword();
+    });
+    test("displays error message when password update fails because user entered incorrect old password", async () => {
+      const user = userEvent.setup();
+
+      const oldPasswordInput = screen.getByLabelText("Old Password");
+      const newPasswordInput = screen.getByLabelText("New Password");
+      const changePasswordButton = screen.getByRole("button", {
+        name: "Change Password",
+      });
+
+      await user.type(oldPasswordInput, "oldpassword");
+      await user.type(newPasswordInput, "newpassword");
+
+      await user.click(changePasswordButton);
+
+      expect(
+        await screen.findByText(/Problem updating password/i)
+      ).toBeInTheDocument();
     });
 
-    vi.mocked(awsAmplifyAuth.updatePassword).mockResolvedValueOnce();
+    test("displays feedback if user tries to submit form without entering old and new passwords", async () => {
+      const user = userEvent.setup();
 
-    renderChangePassword();
+      const oldPasswordInput = screen.getByLabelText(/old password/i);
+      const newPasswordInput = screen.getByLabelText(/new password/i);
+      const oldPasswordInputFeedback = oldPasswordInput.nextElementSibling;
+      const newPasswordInputFeedback = newPasswordInput.nextElementSibling;
 
-    const oldPasswordInput = screen.getByLabelText("Old Password");
-    const newPasswordInput = screen.getByLabelText("New Password");
-    const changePasswordButton = screen.getByRole("button", {
-      name: /change password/i,
+      const changePasswordButton = screen.getByRole("button", {
+        name: /change password/i,
+      });
+
+      await user.click(changePasswordButton);
+
+      expect(oldPasswordInput).toHaveClass("is-invalid");
+      expect(newPasswordInput).toHaveClass("is-invalid");
+      expect(oldPasswordInputFeedback).toHaveTextContent(/required/i);
+      expect(newPasswordInputFeedback).toHaveTextContent(/required/i);
     });
-
-    await user.type(oldPasswordInput, "oldpassword");
-    await user.type(newPasswordInput, "newpassword");
-
-    await user.click(changePasswordButton);
-
-    expect(awsAmplifyAuth.updatePassword).toHaveBeenCalledTimes(1);
-    expect(awsAmplifyAuth.updatePassword).toHaveBeenCalledWith({
-      oldPassword: "oldpassword",
-      newPassword: "newpassword",
-    });
-
-    expect(vi.mocked(toast.success)).toHaveBeenCalledWith(
-      "Password changed successfully"
-    );
-  });
-
-  test("displays error message when password update fails because user entered incorrect old password", async () => {
-    const user = userEvent.setup();
-    const errorMessage = "Incorrect username or password.";
-
-    vi.mocked(awsAmplifyAuth.getCurrentUser).mockResolvedValueOnce({
-      username: "testuser",
-      userId: "123",
-    });
-
-    vi.mocked(awsAmplifyAuth.updatePassword).mockRejectedValueOnce({
-      message: errorMessage,
-    });
-
-    renderChangePassword();
-
-    const oldPasswordInput = screen.getByLabelText("Old Password");
-    const newPasswordInput = screen.getByLabelText("New Password");
-    const changePasswordButton = screen.getByRole("button", {
-      name: "Change Password",
-    });
-
-    await user.type(oldPasswordInput, "oldpassword");
-    await user.type(newPasswordInput, "newpassword");
-
-    await user.click(changePasswordButton);
-
-    expect(
-      await screen.findByText(`Problem updating password: ${errorMessage}`)
-    ).toBeInTheDocument();
-  });
-
-  test("displays feedback if user tries to submit form without entering old and new passwords", async () => {
-    const user = userEvent.setup();
-
-    vi.mocked(awsAmplifyAuth.getCurrentUser).mockResolvedValueOnce({
-      username: "testuser",
-      userId: "123",
-    });
-
-    renderChangePassword();
-
-    const oldPasswordInput = screen.getByLabelText(/old password/i);
-    const newPasswordInput = screen.getByLabelText(/new password/i);
-    const oldPasswordInputFeedback = oldPasswordInput.nextElementSibling;
-    const newPasswordInputFeedback = newPasswordInput.nextElementSibling;
-
-    const changePasswordButton = screen.getByRole("button", {
-      name: /change password/i,
-    });
-
-    await user.click(changePasswordButton);
-
-    expect(oldPasswordInput).toHaveClass("is-invalid");
-    expect(newPasswordInput).toHaveClass("is-invalid");
-    expect(oldPasswordInputFeedback).toHaveTextContent(/required/i);
-    expect(newPasswordInputFeedback).toHaveTextContent(/required/i);
   });
 });
