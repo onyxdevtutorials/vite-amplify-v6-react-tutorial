24b934741835c7197749ddd52d98ff0fd3b4ea5d
David Silva <davidavilasilva@gmail.com>
2024-01-23 12:10:02 -0800

    No longer import vitest globals

--- src/components/ListProducts.test.tsx
+++ src/components/ListProducts.test.tsx
@@ -1,4 +1,3 @@
-import { describe, test, vi, expect, beforeEach } from "vitest";
 import { render, screen, waitFor } from "@testing-library/react";
 import ListProducts from "./ListProducts";
 import { MemoryRouter } from "react-router-dom";
