a674debb60ffc0bd82867fce5651017802144a67
David Silva <davidavilasilva@gmail.com>
2024-01-24 13:26:38 -0800

    Try to fix Lambda error "Cannot use import statement outside a module"

--- amplify/backend/function/authservicesPostConfirmation/src/custom.js
+++ amplify/backend/function/authservicesPostConfirmation/src/custom.js
@@ -1,4 +1,4 @@
-import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
+const { DynamoDBClient, PutItemCommand } = require("@aws-sdk/client-dynamodb");
 
 const ddbClient = new DynamoDBClient({ region: "us-east-1" });
 
