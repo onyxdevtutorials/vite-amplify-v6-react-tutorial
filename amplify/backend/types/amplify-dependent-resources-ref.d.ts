ae22183cf437f1b2e8784fa371587fed38ffe7b9
David Silva <davidavilasilva@gmail.com>
2024-01-24 11:43:26 -0800

    Add post confirmation lambda function

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -17,6 +17,15 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolName": "string"
     }
   },
+  "function": {
+    "authservicesPostConfirmation": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
   "storage": {
     "productimages": {
       "BucketName": "string",
