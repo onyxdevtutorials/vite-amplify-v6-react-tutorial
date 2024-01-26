dba6ebee7f8c1dc31a71a42fe4c068e722260e4a
David Silva <davidavilasilva@gmail.com>
2024-01-26 12:41:32 -0800

    Add coverage reporting

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,5 +6,17 @@ export default defineConfig({
     environment: "jsdom",
     css: true,
     setupFiles: "./src/test/setup.ts",
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html", "json"],
+      exclude: [
+        "**/amplify/**",
+        "**/.*",
+        "src/API.ts",
+        "src/aws-exports.js",
+        "src/vite-env.d.ts",
+        "src/graphql/**",
+      ],
+    },
   },
 });
