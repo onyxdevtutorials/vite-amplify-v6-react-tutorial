33160424c55c11d83cf4fecbaca215bcca20f8e1
David Silva <davidavilasilva@gmail.com>
2024-01-26 12:13:18 -0800

    Make sure main content is below navbar

--- src/pages/HomeLayout.tsx
+++ src/pages/HomeLayout.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom";
 import { Banner } from "../components";
 import Container from "react-bootstrap/Container";
+import "./HomeLayout.css";
 
 const HomeLayout = () => {
   return (
     <>
       <Banner />
-      <main className="mt-3">
+      <main className="main-content">
         <Container fluid>
           <Outlet />
         </Container>
