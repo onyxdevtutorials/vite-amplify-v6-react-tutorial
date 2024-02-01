df2e94af1ac019952cbde1c9a65be66b8e9f45ed
David Silva <davidavilasilva@gmail.com>
2024-01-31 16:47:35 -0800

    Create an image upload component

---
+++ src/components/ImageUpload.tsx
@@ -0,0 +1,81 @@
+import React, { useCallback, useMemo } from "react";
+import { useDropzone } from "react-dropzone";
+
+type ImageUploadProps = {
+  onFileSelect: (file: File) => void;
+};
+
+const baseStyle: React.CSSProperties = {
+  flex: 1,
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  padding: "20px",
+  borderWidth: 2,
+  borderRadius: 2,
+  borderColor: "#eeeeee",
+  borderStyle: "dashed",
+  backgroundColor: "#fafafa",
+  color: "#bdbdbd",
+  outline: "none",
+  transition: "border .24s ease-in-out",
+};
+
+const focusedStyle = {
+  borderColor: "#2196f3",
+};
+
+const acceptStyle = {
+  borderColor: "#00e676",
+};
+
+const rejectStyle = {
+  borderColor: "#ff1744",
+};
+
+const ImageUpload: React.FC<ImageUploadProps> = ({ onFileSelect }) => {
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    if (acceptedFiles.length > 1) {
+      console.error("Only one file can be uploaded at a time");
+      return;
+    }
+
+    const file = acceptedFiles[0];
+    onFileSelect(file);
+  }, []);
+
+  const {
+    getRootProps,
+    getInputProps,
+    isFocused,
+    isDragAccept,
+    isDragReject,
+    isDragActive,
+  } = useDropzone({
+    onDrop,
+    accept: { "image/jpeg": [], "image/png": [] },
+    multiple: false,
+  });
+
+  const style = useMemo(
+    () => ({
+      ...baseStyle,
+      ...(isFocused ? focusedStyle : {}),
+      ...(isDragAccept ? acceptStyle : {}),
+      ...(isDragReject ? rejectStyle : {}),
+    }),
+    [isFocused, isDragAccept, isDragReject]
+  );
+
+  return (
+    <div {...getRootProps({ style })}>
+      <input {...getInputProps()} />
+      {isDragActive ? (
+        <p>Drop the files here ...</p>
+      ) : (
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      )}
+    </div>
+  );
+};
+export default ImageUpload;
